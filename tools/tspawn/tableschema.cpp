b3d17d172fe827588950fba348baf5c289abea2b
Sam Kenyon <sam@metadevo.com>
2018-07-07 23:19:52 -0400

    Fix the SQL for child tables and set the fields to the correct ones.

--- tools/tspawn/tableschema.cpp
+++ tools/tspawn/tableschema.cpp
@@ -283,17 +283,20 @@ QMap<QString, QPair<QString, QString>> TableSchema::getParentTables() const
     return map;
 }
 
+
+
+
 QMap<QPair<QString, QString>, QString> TableSchema::getChildTables() const
 {
     QMap<QPair<QString, QString>, QString> map;
     QSqlQuery query(QSqlDatabase::database());
     query.prepare(
-            "SELECT tc.table_schema, tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_schema AS foreign_table_schema, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name WHERE constraint_type = 'FOREIGN KEY' AND ccu.table_name='" +
+            "SELECT tc.table_schema, tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_schema AS foreign_table_schema, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='" +
             tablename + "'");
     query.exec();
     while (query.next()) {
-        QPair<QString, QString> qp(query.value(2).toString(), query.value(3).toString());
-        map[qp] = query.value(6).toString();
+        QPair<QString, QString> qp(query.value(5).toString(), query.value(6).toString());
+        map[qp] = query.value(3).toString();
     }
     return map;
 }
